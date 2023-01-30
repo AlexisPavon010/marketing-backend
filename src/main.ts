59ccbe2f59b81fa500b06db792ce5f48b0c2a35a
Alexis Pavon <themaster034@gmail.com>
2023-01-30 09:34:29 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://next-store-liard-three.vercel.app'],
+    allowedHeaders: ['Accept', 'Content-Type'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.setGlobalPrefix('api')
   app.useGlobalPipes(
     new ValidationPipe({
